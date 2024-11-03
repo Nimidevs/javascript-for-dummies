fbdd80cd9581b9a75b69e9aa40e875596b93b0a6
Nimidevs <106442956+Nimidevs@users.noreply.github.com>
2024-11-03 19:42:16 +0100

    Local creation of a repo

---
+++ chapter3.js
@@ -0,0 +1,11 @@
+let tip = "8.5";
+let tax = 2;
+let total = 90;
+
+let orderTotal = tip + tax + total;
+
+let orderTotalNumberOne = tax + total + tip;
+
+let orderTotalNumberTwo = tax + tip + total;
+
+console.log(orderTotal, orderTotalNumberOne, orderTotalNumberTwo, (8.5+2+90));
